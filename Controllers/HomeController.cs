c9b2dd54f203fad8b38e97d57cf8fb92afc1da1a
Anduin2017 <Anduin@aiursoft.com>
2018-01-22 20:15:52 +0800

    Create error handler.

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -10,12 +10,18 @@ using Aiursoft.Pylon.Models;
 
 namespace Kahla.Server.Controllers
 {
-    [AiurRequireHttps]
+    [AiurRequireHttps]    
+    [AiurAllowTargetOrigin("https://kahla.app.aiursoft.com")]
     public class HomeController : AiurController
     {
         public IActionResult Index()
         {
             return Protocal(ErrorType.Success, "Welcome to kahla server! View our wiki at: " + Values.WikiServerAddress);
+        }
+
+        public IActionResult Error()
+        {
+            return Protocal(ErrorType.UnknownError, "Kahla server crushed! Please tell us!");
         }
     }
 }
