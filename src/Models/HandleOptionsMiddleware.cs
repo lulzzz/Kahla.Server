0bfadf8b71238b2bb67dcb6f0fa71dd1b9d85ff7
Anduin2017 <anduin@aiursoft.com>
2018-03-11 23:56:55 +0800

    Add header even in normal requests.

--- src/Models/HandleOptionsMiddleware.cs
+++ src/Models/HandleOptionsMiddleware.cs
@@ -17,20 +17,20 @@ namespace Kahla.Server.Models
 
         public Task Invoke(HttpContext context)
         {
+            context.Response.Headers.Add("Access-Control-Allow-Headers", "Authorization");
+            if (context.Request.Path.Value.ToLower().Contains("debug"))
+            {
+                context.Response.Headers.Add("Access-Control-Allow-Origin", "http://localhost:8001");
+                context.Response.Headers.Add("Access-Control-Allow-Credentials", "true");
+            }
+            else
+            {
+                context.Response.Headers.Add("Access-Control-Allow-Origin", "https://kahla.app.aiursoft.com");
+                context.Response.Headers.Add("Access-Control-Allow-Credentials", "true");
+            }
             if (context.Request.Method == "OPTIONS")
             {
                 context.Response.StatusCode = 204;
-                context.Response.Headers.Add("Access-Control-Allow-Headers", "Authorization");
-                if (context.Request.Path.Value.ToLower().Contains("debug"))
-                {
-                    context.Response.Headers.Add("Access-Control-Allow-Origin", "http://localhost:8001");
-                    context.Response.Headers.Add("Access-Control-Allow-Credentials", "true");
-                }
-                else
-                {
-                    context.Response.Headers.Add("Access-Control-Allow-Origin", "https://kahla.app.aiursoft.com");
-                    context.Response.Headers.Add("Access-Control-Allow-Credentials", "true");
-                }
                 return context.Response.WriteAsync("This response allow browser to send requests.");
             }
             return _next.Invoke(context);
