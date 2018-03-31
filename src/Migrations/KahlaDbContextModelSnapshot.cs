a65765b79868f87aeb1cf4a024b30601c8f5e4ab
Anduin2017 <anduin@aiursoft.com>
2018-03-31 14:28:54 +0800

    UPgrade ef migration version

--- src/Migrations/KahlaDbContextModelSnapshot.cs
+++ src/Migrations/KahlaDbContextModelSnapshot.cs
@@ -18,7 +18,7 @@ namespace Kahla.Server.Migrations
         {
 #pragma warning disable 612, 618
             modelBuilder
-                .HasAnnotation("ProductVersion", "2.0.1-rtm-125")
+                .HasAnnotation("ProductVersion", "2.0.2-rtm-10011")
                 .HasAnnotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn);
 
             modelBuilder.Entity("Kahla.Server.Models.Conversation", b =>
